1dd9eb5b8b45724e635ab63774725b751387e3e4
Maxime Mongeau <mongeaumax@gmail.com>
2016-02-13 02:48:03 -0500

    Basic form with location selector

--- src/AddBAFormViewController.swift
+++ src/AddBAFormViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 import Eureka
+import MapKit
 
 class AddBAFormViewController: FormViewController {
     
+    let user: String = "Super user"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        form +++= TextRow("Titre"){$0.title = "Titre"}
+        title = "Nouvelle BA"
+        form
+            +++= TextRow("title"){
+                $0.title = "Titre"
+                $0.placeholder = "Requis"
+            }
+            <<< TextAreaRow("description"){$0.placeholder = "Description"}
+            <<< DateTimeInlineRow("startDate"){$0.title = "Début"}
+            <<< DateTimeInlineRow("endDate"){$0.title = "Fin"}
+            <<< IntRow("nbBenevole"){$0.title = "Nombre de bénévoles"}
+            <<< LocationRow("location"){
+                $0.title = "Lieu"
+            }
+    }
+    
+    @IBAction func cancelPressed(sender: UIBarButtonItem) {
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
 }
