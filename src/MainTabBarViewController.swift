d3253352ddd8dfa121a3ac980d1ba153fa569e35
Samuel Bellerose <samuel.bellerose@icloud.com>
2016-02-12 23:13:14 -0500

    Fixed ViewController on app start

---
+++ src/MainTabBarViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabBarViewController.swift
+//  BA-iOS-Application
+//
+//  Created by Samuel Bellerose on 2016-02-12.
+//  Copyright © 2016 Samuel Bellerose. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+    
+}
