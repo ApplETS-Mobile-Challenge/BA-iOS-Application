6fdac242808fa753b7b7867a77e3786d8f3d65b2
Vincent Dupuis <vincent.dupuis.lemelin@gmail.com>
2016-02-12 23:57:46 -0500

    Début des modèles

---
+++ src/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  BA-iOS-Application
+//
+//  Created by Vincent Dupuis on 2016-02-12.
+//  Copyright © 2016 Samuel Bellerose. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
